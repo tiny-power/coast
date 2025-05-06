1611a3e277d3af9a18271226401c2d3a69f16644
tiny <tinybrief@gmail.com>
2025-05-06 11:00:49 +0800

    added source-code-pro font

--- src/views/home.vue
+++ src/views/home.vue
@@ -2557,7 +2557,7 @@ export default {
             item.xterm = new Terminal({
                 cursorBlink: this.platform === 'win32' ? true : false,
                 cursorStyle: this.platform === 'win32' ? 'underline' : 'block',
-                fontFamily: 'monospace',
+                fontFamily: 'SourceCodePro',
                 fontSize: 14,
                 lineHeight: 1.2,
                 theme: this.theme
